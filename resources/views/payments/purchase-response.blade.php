9576537cf758b2cc54b366cb50f3af66f1daf2b7
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-07-19 16:50:15 -0600

    fix comprobante

--- resources/views/payments/purchase-response.blade.php
+++ resources/views/payments/purchase-response.blade.php
@@ -9,7 +9,7 @@
 			<h1>Gracias por tu compra!!</h1>
 			<clean-cart :cart.sync="cart" :cart-sub-total.sync="cartSubTotal" :tax="tax" :cart-total.sync="cartTotal" ></clean-cart>
 			
-			<h4>Comprobante: {{ $orde_id }}</h4>
+			<h4>Comprobante: {{ $order_id }}</h4>
 			<order :request="{{ $order }}" >Este es tu numero de orden</order>
 		</div>
 </section>
