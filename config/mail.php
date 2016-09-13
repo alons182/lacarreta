6ea06ea7c78dc0bcda896ea4cf61b7e88482a1d8
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-09-12 18:51:33 -0600

    cambio de sender mail

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'noreply@grupolacarreta.com',
+        'name' => 'Grupo la Carreta',
     ],
 
     /*
